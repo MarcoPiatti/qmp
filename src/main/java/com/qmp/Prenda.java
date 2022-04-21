2acbbfcd6523301d92c3aa64d370dc759d4b89d0
Marco Piatti <marcopiatticastro@hotmail.com>
2022-04-21 01:11:21 -0300

    colorSecundario ahora es Optional

--- src/main/java/com/qmp/Prenda.java
+++ src/main/java/com/qmp/Prenda.java
@@ -2,18 +2,19 @@ package com.qmp;
 
 import java.awt.Color;
 import java.util.Objects;
+import java.util.Optional;
 
 public class Prenda {
   TipoPrenda tipo;
   String material;
   Color colorPrimario;
-  Color colorSecundario;
+  Optional<Color> colorSecundario;
 
   public Prenda(TipoPrenda tipo, String material, Color colorPrimario, Color colorSecundario) {
     this.tipo = Objects.requireNonNull(tipo, "El tipo de prenda no puede ser nulo");
     this.material = Objects.requireNonNull(material, "El material no puede ser nulo");
     this.colorPrimario = Objects.requireNonNull(colorPrimario, "El color primario no puede ser nulo");
-    this.colorSecundario = colorSecundario;
+    this.colorSecundario = Optional.ofNullable(colorSecundario);
   }
 
   public CategoriaPrenda getCategoria() {
---
+++ src/test/java/com/qmp/PrendaTest.java
@@ -0,0 +1,51 @@
+package com.qmp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.awt.Color;
+
+public class PrendaTest {
+  
+  @Test
+  public void unaPrendaNoDebeFaltarTipo(){
+    assertThrows(NullPointerException.class, () -> prendaSinTipo());
+  }
+
+  @Test
+  public void unaPrendaNoDebeFaltarMaterial(){
+    assertThrows(NullPointerException.class, () -> prendaSinMaterial());
+  }
+
+  @Test
+  public void unaPrendaNoDebeFaltarColorPrimario(){
+    assertThrows(NullPointerException.class, () -> prendaSinColorPrimario());
+  }
+
+  @Test
+  public void unaPrendaPuedeNoTenerColorSecundario(){
+    assertFalse(prendaSinColorSecundario().colorSecundario.isPresent());
+  }
+
+  private Prenda prendaSinTipo() {
+    return new Prenda(null, "algodon", Color.BLACK, Color.WHITE);
+  }
+
+  private Prenda prendaSinMaterial() {
+    return new Prenda(unTipoPrendaCualquiera(), null, Color.BLACK, Color.WHITE);
+  }
+
+  private Prenda prendaSinColorPrimario() {
+    return new Prenda(unTipoPrendaCualquiera(), "algodon", null, Color.WHITE);
+  }
+
+  private Prenda prendaSinColorSecundario() {
+    return new Prenda(unTipoPrendaCualquiera(), "algodon", Color.BLACK, null);
+  }
+
+  private TipoPrenda unTipoPrendaCualquiera() {
+    return new TipoPrenda("pantaloncito", CategoriaPrenda.INFERIOR);
+  }
+
+}
