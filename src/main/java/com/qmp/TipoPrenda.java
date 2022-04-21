7cb274940eed6e6f1c126f092c7f2286e55abcac
Marco Piatti <marcopiatticastro@hotmail.com>
2022-04-21 01:11:51 -0300

    agregadas validaciones a tipoprenda

--- src/main/java/com/qmp/TipoPrenda.java
+++ src/main/java/com/qmp/TipoPrenda.java
@@ -1,12 +1,14 @@
 package com.qmp;
 
+import java.util.Objects;
+
 public class TipoPrenda {
   String nombre;
   CategoriaPrenda categoria;
 
   public TipoPrenda(String nombre, CategoriaPrenda categoria) {
-    this.nombre = nombre;
-    this.categoria = categoria;
+    this.nombre = Objects.requireNonNull(nombre, "El nombre de un TipoPrenda no puede ser nulo");
+    this.categoria = Objects.requireNonNull(categoria, "La categoria de un TipoPrenda no puede ser nula");
   } 
 
   public CategoriaPrenda getCategoria() {
